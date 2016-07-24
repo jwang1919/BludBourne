3ddf83abc82da3107ece1159665f11513a241e94
Jesse Wang <Jesse Wang>
2016-07-24 13:57:50 -0400

    initial commit

---
+++ android/src/com/jwang1919/bludbourne/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jwang1919.bludbourne;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jwang1919.bludbourne.BludBourne;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BludBourne(), config);
+	}
+}
---
+++ html/src/com/jwang1919/bludbourne/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.jwang1919.bludbourne.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.jwang1919.bludbourne.BludBourne;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new BludBourne();
+        }
+}
